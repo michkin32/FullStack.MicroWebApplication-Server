fa4d9378a33f26ac7323766a83dedb8c6a87295e
Kievina <56592501+Kievina@users.noreply.github.com>
2019-12-13 11:03:38 -0500

    add logic to getMessages by chatid and userid

--- src/main/java/com/groupfour/chatapp/chatapp/message/MessageRepository.java
+++ src/main/java/com/groupfour/chatapp/chatapp/message/MessageRepository.java
@@ -1,12 +1,13 @@
 package com.groupfour.chatapp.chatapp.message;
 
+import com.groupfour.chatapp.chatapp.chat.Chat;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MessageRepository extends CrudRepository<Message, Long> {
-
-//Message findByUser();
-
-
+    Iterable<Message> findMessagesBySender(Long userId);
+    Iterable<Message> findMessagesByDestinationChat(Long chatId);
 }
