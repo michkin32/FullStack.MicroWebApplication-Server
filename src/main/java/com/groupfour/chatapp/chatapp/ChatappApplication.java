3adf5d9095868a9fd4bafaf2ec3196af533cea48
Mike King <michkin32@gmail.com>
2019-12-20 01:57:44 -0500

    Adding eddies poll code and file transfer endpoints

--- src/main/java/com/groupfour/chatapp/chatapp/ChatappApplication.java
+++ src/main/java/com/groupfour/chatapp/chatapp/ChatappApplication.java
@@ -1,9 +1,14 @@
 package com.groupfour.chatapp.chatapp;
 
+import com.groupfour.chatapp.chatapp.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class ChatappApplication {
 
 	public static void main(String[] args) {
