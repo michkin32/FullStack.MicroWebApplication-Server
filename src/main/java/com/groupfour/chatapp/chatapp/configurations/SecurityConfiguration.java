acfdd2d4fafe32f38fb0822e12cf6147d2b3eaa3
mike <mike.schoenbeck@gmail.com>
2020-01-01 19:53:47 -0500

    branched off of shortpassreset2C to start working on user login feature. This branch includes all existing registration activity

--- src/main/java/com/groupfour/chatapp/chatapp/configurations/SecurityConfiguration.java
+++ src/main/java/com/groupfour/chatapp/chatapp/configurations/SecurityConfiguration.java
@@ -9,6 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+
+
+
+//
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
