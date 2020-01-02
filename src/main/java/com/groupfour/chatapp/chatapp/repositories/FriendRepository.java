31c180f50c00908a080b41cb99ac9f5d0527b443
eddie <eddiem9657@gmail.com>
2020-01-02 13:33:51 -0500

    Repo annotation wasnt added, oops

--- src/main/java/com/groupfour/chatapp/chatapp/repositories/FriendRepository.java
+++ src/main/java/com/groupfour/chatapp/chatapp/repositories/FriendRepository.java
@@ -2,6 +2,8 @@ package com.groupfour.chatapp.chatapp.repositories;
 
 import com.groupfour.chatapp.chatapp.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FriendRepository extends CrudRepository {
 }
