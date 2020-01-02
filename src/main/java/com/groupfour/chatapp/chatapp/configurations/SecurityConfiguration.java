41466d7e7af0b65f5c24abf34e65c270ba422be4
mike <mike.schoenbeck@gmail.com>
2020-01-01 20:59:16 -0500

    added configure method to securityconfiguration to allow for login page.

--- src/main/java/com/groupfour/chatapp/chatapp/configurations/SecurityConfiguration.java
+++ src/main/java/com/groupfour/chatapp/chatapp/configurations/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.groupfour.chatapp.chatapp.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,12 +13,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
 
-//
+// I believe WebSecurityConfigurerAdapter secures the entire website and the configure methods
+//  determine what roles and what endpoints are accessible.
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/register").permitAll()
-                .antMatchers("/confirm").permitAll();
+                .antMatchers("/confirm").permitAll()
+                .antMatchers("/login").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+        throws Exception{
+        auth
+                .inMemoryAuthentication()
+            .withUser("user")
+            .password("password");
+           // .roles("USER");       We are not using roles now. Probably won't ever.
     }
 }
