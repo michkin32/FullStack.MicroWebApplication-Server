a99985ef63d843be4aef9f4b6a68491428765e25
Ryan <ryan@rmiller.dev>
2020-01-03 16:43:12 -0500

    cors

--- src/main/java/com/groupfour/chatapp/chatapp/controllers/UserController.java
+++ src/main/java/com/groupfour/chatapp/chatapp/controllers/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin
 @RestController
 public class UserController {
     private UserService userService;
