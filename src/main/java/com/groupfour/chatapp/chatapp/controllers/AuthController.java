25aff0bddd90da456637cf4ab1057936f537bee7
mike <mike.schoenbeck@gmail.com>
2020-01-04 19:08:05 -0500

    fixed userdetailservice circular reference by adding userdetailsservice after the class @service been for userserviceimpl. Now working on 403 error when trying to log in

--- src/main/java/com/groupfour/chatapp/chatapp/controllers/AuthController.java
+++ src/main/java/com/groupfour/chatapp/chatapp/controllers/AuthController.java
@@ -14,8 +14,13 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
-    @Autowired
+
     private AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     @PostMapping("/api/auth/register")
     public ResponseEntity signup(@RequestBody RegisterRequest registerRequest) {
