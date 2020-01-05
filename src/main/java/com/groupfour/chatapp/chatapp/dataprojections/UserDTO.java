f286bfd538e5864e1a3aff593c5818fb1656e9b8
Mike King <michkin32@gmail.com>
2020-01-05 18:28:40 -0500

    change added to user DTO

--- src/main/java/com/groupfour/chatapp/chatapp/dataprojections/UserDTO.java
+++ src/main/java/com/groupfour/chatapp/chatapp/dataprojections/UserDTO.java
@@ -3,5 +3,6 @@ package com.groupfour.chatapp.chatapp.dataprojections;
 public interface UserDTO {
     Long getUserId();
     String getUserName();
+    String getProfilePic();
     Integer getActiveStatus();
 }
